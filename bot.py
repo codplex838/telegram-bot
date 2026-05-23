5c913584d50029d9c437b3f26f37ecf2ea0166ce
codplex838 <saipavanmahankali53@gmail.com>
2026-05-23 14:56:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,25 @@
-from telegram.ext import ApplicationBuilder, CommandHandler
 import os
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+)
 
-TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-async def start(update, context):
-    await update.message.reply_text("Hello! Bot is running on Render.")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Hello! Bot is running successfully on Render!"
+    )
 
-app = ApplicationBuilder().token(TOKEN).build()
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("start", start))
 
-print("Bot started...")
-app.run_polling()
+    print("Bot started successfully...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
