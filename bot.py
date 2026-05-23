b88fa151a7389d0284706c302c5feffff61413d6
codplex838 <saipavanmahankali53@gmail.com>
2026-05-23 17:14:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,47 +1,14 @@
+import os
 from telegram import Update
-from telegram.ext import (
-    Application,
-    CommandHandler,
-    MessageHandler,
-    ContextTypes,
-    filters,
-)
+from telegram.ext import Application, CommandHandler, ContextTypes
 
-from pymongo import MongoClient
-import uuid
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-BOT_TOKEN = "7313598031:AAHpI5-UCF3Cyw2QwhiV0gyTUR41oiIvcFY"
-
-MONGO_URL = "mongodb+srv://srinivasaraomahankali97_db_user:Ryx3jbIoh036Nbkc@bread.roaqkqu.mongodb.net/?appName=BREAD"
-
-client = MongoClient(MONGO_URL)
-
-db = client["telegram_bot"]
-
-collection = db["videos"]
+RENDER_URL = os.getenv("RENDER_EXTERNAL_URL")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "Send me a video."
-    )
-
-async def handle_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    video = update.message.video
-
-    unique_id = str(uuid.uuid4())[:8]
-
-    data = {
-        "unique_id": unique_id,
-        "file_id": video.file_id,
-        "file_name": video.file_name,
-        "file_size": video.file_size,
-        "uploader": update.effective_user.id
-    }
-
-    collection.insert_one(data)
-
-    await update.message.reply_text(
-        f"✅ Saved Successfully!\n\nID: {unique_id}"
+        "Hello! Professional webhook bot is running!"
     )
 
 def main():
@@ -49,13 +16,13 @@ def main():
 
     app.add_handler(CommandHandler("start", start))
 
-    app.add_handler(
-        MessageHandler(filters.VIDEO, handle_video)
+    print("Webhook bot started...")
+
+    app.run_webhook(
+        listen="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000)),
+        webhook_url=f"{RENDER_URL}/{BOT_TOKEN}"
     )
-
-    print("MongoDB Bot Started...")
-
-    app.run_polling()
 
 if __name__ == "__main__":
     main()
