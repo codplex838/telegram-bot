6466d737fee3ca67085f5a95ace24148cf610e16
codplex838 <saipavanmahankali53@gmail.com>
2026-05-23 16:35:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,26 @@
-import os
 from telegram import Update
 from telegram.ext import (
     Application,
     CommandHandler,
+    MessageHandler,
     ContextTypes,
+    filters,
 )
 
 BOT_TOKEN = "7313598031:AAHpI5-UCF3Cyw2QwhiV0gyTUR41oiIvcFY"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "Hello! Bot is running successfully on Render!"
+        "Send me any video and I will return its Telegram file_id."
+    )
+
+async def handle_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    video = update.message.video
+
+    file_id = video.file_id
+
+    await update.message.reply_text(
+        f"FILE ID:\n\n{file_id}"
     )
 
 def main():
@@ -18,7 +28,12 @@ def main():
 
     app.add_handler(CommandHandler("start", start))
 
+    app.add_handler(
+        MessageHandler(filters.VIDEO, handle_video)
+    )
+
     print("Bot started successfully...")
+
     app.run_polling()
 
 if __name__ == "__main__":
